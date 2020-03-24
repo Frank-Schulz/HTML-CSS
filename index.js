9a51d4aa6d55dc426a21e6f1cd2487d762305bc9
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-03-24 16:18:59 +1300

    further work on CSS Grid

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("navbar");
-var btns = header.getElementsByClassName("btn");
+// Get the container element
+var btnContainer = document.getElementById("navbar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the current class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("current");
-  current[0].className = current[0].className.replace(" current", "");
-  this.className += " current";
+    var current = document.getElementsByClassName("current");
+
+    // If there's no current class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" current", "");
+    }
+
+    // Add the current class to the current/clicked button
+    this.className += " current";
   });
 }
